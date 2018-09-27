0caf4c4fee6ca8c8d8fa8498a93475a964fd5dea
free <303152444@qq.com>
2018-09-27 18:13:26 +0800

    chapter 17 update

---
+++ 17-chapter/15-17.c
@@ -0,0 +1,86 @@
+#include "apue.h"
+#include <sys/socket.h>
+
+#define CMGROUP_MAX	16
+struct cmsgcred{
+	pid_t cmcred_pid;
+	uit_t cmcred_uid;
+	uid_t cmcred_euid;
+	git_t cmcred_gid;
+	short cmcred_ngroups;
+	gid_t cmcred_groups[CMGROUP_MAX];
+};
+
+struct ucred{
+	pid_t	pid;
+	uid_t	uid;
+	gid_t	gid;
+};
+
+#if defined(SCM_CREDS)
+#define CREDSTRUCT		cmsgcred
+#define	SCM_CREDTYPE	SCM_CREDS	
+#elif defined(SCM_CREDENTIALS)
+#define	CREDSTRUCT		ucred
+#define SCM_CREDTYPE	SCM_CREDENTIALS
+#else
+#error passing credentials is unsupported!
+#endif
+
+#define RIGHTSLEN	CMSG_LEN(sizeof(int))
+#define	CREDSLEN	CMSG_LEN(sizeof(struct CREDSTRUCT))
+#define	CONTROLLEN	(RIGHTSLEN + CREDSLEN)
+
+static struct cmsghdr *cmptr = NULL;
+
+int send_fd(int fd, int fd_to_send)
+{
+	struct CREDSTRUCT	*credp;
+	struct cmsghdr		*cmp;
+	struct iovec		iov[1];
+	struct msghdr		msg;
+	char				buf[2];
+
+	iov[0].iov_base = buf;
+	iov[0].iov_len  = 2;
+	msg.msg_iov     = iov;
+	msg.msg_iovlen	= 1;
+	msg.msg_name	= NULL;
+	msg.msg_namelen = 0;
+	msg.msg_flags	= 0;
+	if(fd_to_send < 0)
+	{
+		msg.msg_control = NULL;
+		msg.msg_controllen	= 0;
+		buf[1] = -fd_to_send;
+		if(buf[1] == 0)
+			buf[1] = 1;
+	}
+	else
+	{
+		if(cmptr == NULL && (cmptr = malloc(CONTROLLEN)) == NULL)
+			return(-1);
+		msg.msg_control = cmptr;
+		msg.msg_controllen = CONTROLLEN;
+		cmp = cmptr;
+		cmp->cmsg_level = SOL_SOCKET;
+		cmp->cmsg_type	= SCM_RIGHTS;
+		cmp->cmsg_len	= RIGHTSLEN;
+		*(int *)CMSG_DATA(cmp) = fd_to_send;
+		cmp = CMSG_NXTHDR(&msg, cmp);
+		cmp->cmsg_level = SOL_SOCKET;
+		cmp->cmsg_type	= SCM_CREDTYPE;
+		cmp->cmsg_len	= CREDSLEN;
+		credp = (struct CREDSTRUCT *)CMSG_DATA(cmp);
+#if defined(SCM_CREDENTIALS)
+		credp->uid = getuid();
+		credp->gid = getegid();
+		credp->pid = getpid();
+#endif
+		buf[1] = 0;
+	}
+	buf[0] = 0;
+	if(sendmsg(fd, &msg, 0) != 2)
+		return(-1);
+	return(0);
+}
